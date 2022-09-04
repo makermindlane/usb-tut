38aa8fe5473e5119bb53fa925aea7eedbd17ead6
Pawan <makermindlane@gmail.com>
2022-09-05 00:10:41 +0530

    + init_gpio_pins()

--- Src/usbd_driver.c
+++ Src/usbd_driver.c
@@ -1,1 +1,14 @@
 #include "usbd_driver.h"
+
+void init_gpio_pins() {
+    // Enable the clock for GPIOA.
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+
+    // Set alternate function 10 for PA11 (data minus) PA12 (data plus).
+    MODIFY_REG(GPIOA->AFR[1], GPIO_AFRH_AFSEL11 | GPIO_AFRH_AFSEL12,
+               _VAL2FLD(GPIO_AFRH_AFSEL11, 0xa) | _VAL2FLD(GPIO_AFRH_AFSEL12, 0xa));
+
+    // Configure pins to work in above set alternate function mode.
+    MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODER11 | GPIO_MODER_MODER12,
+               _VAL2FLD(GPIO_MODER_MODER11, 0x2) | _VAL2FLD(GPIO_MODER_MODER12, 0x2));
+}
