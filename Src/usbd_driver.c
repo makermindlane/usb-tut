a21db2e84e6730c052471969f3b951be40cea733
Pawan <makermindlane@gmail.com>
2022-09-18 22:38:29 +0530

    usbd_driver.c: Added gintsts_handler(), usbrst_handler(), in_endpoint(), out_endpoint(), configure_endpoint0() and configure_endpoint()

--- Src/usbd_driver.c
+++ Src/usbd_driver.c
@@ -1,13 +1,34 @@
 #include "usbd_driver.h"
 
+#include "data_typedefs.h"
+#include "usb_standards.h"
+
 static USB_OTG_GlobalTypeDef *usb_otg_global =
     ((USB_OTG_GlobalTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_GLOBAL_BASE));
 static USB_OTG_DeviceTypeDef *usb_otg_device =
     ((USB_OTG_DeviceTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_DEVICE_BASE));
 
 // Power and clock gating control register
-__IO uint32_t *USB_OTG_FS_PCGCCTL =
-    ((__IO uint32_t *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_PCGCCTL_BASE));
+__IO u32 *USB_OTG_FS_PCGCCTL =
+    ((__IO u32 *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_PCGCCTL_BASE));
+
+static const u8 ENDPOINT_COUNT = 4;
+
+//=================================================================================================
+// Private Functions Prototypes
+//=================================================================================================
+
+static inline USB_OTG_INEndpointTypeDef *in_endpoint(const u8 endpoint_size);
+static inline USB_OTG_OUTEndpointTypeDef *out_endpoint(const u8 endpoint_size);
+static void usbrst_handler();
+static void configure_endpoint0(const u8 endpoint_size);
+static void configure_in_endpoint(const u8 endpoint_number,
+                                  const UsbEndpointType endpoint_type,
+                                  const u16 endpoint_size);
+
+//=================================================================================================
+// Public Functions
+//=================================================================================================
 
 void init_gpio_pins() {
     // Enable the clock for GPIOA.
@@ -65,4 +86,64 @@ void disconnect() {
 
     // Clear the power down disable bit to enable power down and save power.
     CLEAR_BIT(usb_otg_global->GCCFG, USB_OTG_GCCFG_PWRDWN);
+}
+
+void gintsts_handler() {
+    __IO u32 gintsts = usb_otg_global->GINTSTS;
+
+    if (gintsts & USB_OTG_GINTSTS_USBRST) {
+        usbrst_handler();
+        SET_BIT(usb_otg_global->GINTSTS, USB_OTG_GINTSTS_USBRST);
+    } else if (gintsts & USB_OTG_GINTSTS_ENUMDNE) {
+    } else if (gintsts & USB_OTG_GINTSTS_IEPINT) {
+    } else if (gintsts & USB_OTG_GINTSTS_OEPINT) {
+    } else if (gintsts & USB_OTG_GINTSTS_RXFLVL) {
+    }
+}
+
+//=================================================================================================
+// Private Functions
+//=================================================================================================
+
+static inline USB_OTG_INEndpointTypeDef *in_endpoint(const u8 endpoint_size) {
+    return ((USB_OTG_INEndpointTypeDef *)(USB_OTG_FS_PERIPH_BASE +
+                                          USB_OTG_IN_ENDPOINT_BASE +
+                                          (0x20 * endpoint_size)));
+}
+
+static inline USB_OTG_OUTEndpointTypeDef *out_endpoint(const u8 endpoint_size) {
+    return ((USB_OTG_OUTEndpointTypeDef *)(USB_OTG_FS_PERIPH_BASE +
+                                           USB_OTG_OUT_ENDPOINT_BASE +
+                                           (0x20 * endpoint_size)));
+}
+
+static void usbrst_handler() {
+    for (u8 i = 0; i < ENDPOINT_COUNT; ++i) {
+    }
+}
+
+static void configure_endpoint0(const u8 endpoint_size) {
+    // Unmask all interrupt of IN and OUT of endpoint0.
+    SET_BIT(usb_otg_device->DAINTMSK, 1 << 0 | 1 << 16);
+
+    // Configures maximum packet size, activates the endpoint, set nack
+    MODIFY_REG(in_endpoint(0)->DIEPCTL, USB_OTG_DIEPCTL_MPSIZ,
+               USB_OTG_DIEPCTL_USBAEP | _VAL2FLD(USB_OTG_DIEPCTL_MPSIZ, endpoint_size) |
+                   USB_OTG_DIEPCTL_SNAK);
+
+    // Enables endpoint data transmission by clearing the nack.
+    SET_BIT(out_endpoint(0)->DOEPCTL, USB_OTG_DOEPCTL_EPENA | USB_OTG_DOEPCTL_CNAK);
+}
+
+static void configure_in_endpoint(const u8 endpoint_number,
+                                  const UsbEndpointType endpoint_type,
+                                  const u16 endpoint_size) {
+    // Unmask all interrupts of endpoint passed.
+    SET_BIT(usb_otg_device->DAINTMSK, 1 << endpoint_number);
+
+    MODIFY_REG(in_endpoint(endpoint_number)->DIEPCTL,
+               USB_OTG_DIEPCTL_MPSIZ | USB_OTG_DIEPCTL_EPTYP,
+               USB_OTG_DIEPCTL_USBAEP | _VAL2FLD(USB_OTG_DIEPCTL_MPSIZ, endpoint_size) |
+                   _VAL2FLD(USB_OTG_DIEPCTL_EPTYP, endpoint_type) | USB_OTG_DIEPCTL_SNAK |
+                   USB_OTG_DIEPCTL_SD0PID_SEVNFRM);
 }
