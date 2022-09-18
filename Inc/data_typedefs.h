e56696abe70cc95052f55d537d85391ad3149930
Pawan <makermindlane@gmail.com>
2022-09-18 12:48:12 +0530

    Added data_typedefs.h file

---
+++ Inc/data_typedefs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+
+// Why create these typedefs? To ease code writing.
+
+typedef int8_t i8;
+typedef int8_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint8_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef long double f64;
